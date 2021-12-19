284d88b77384a4ac14b5117c826d9bda38c6f86e
Chan <chanhurcaw@gmail.com>
2021-12-19 14:33:50 -0800

    Basic visuals. Implement interactactivity js

--- calculation.js
+++ calculation.js
@@ -1,44 +1,78 @@
 
 function add(x,y){
-    return x + y;
+    return parseInt(x) + parseInt(y);
 }
 
 
 function subtract(x,y){
-    return x - y;
+    return parseInt(x) - parseInt(y);
 }
 
 
 function multiply(x,y){
-    return x * y;
+    return parseInt(x) * parseInt(y);
 }
 
 
 function divide(x,y){
-    return x / y;
+    return parseInt(x) / parseInt(y);
 }
 
-function operate(x, operator, y){
+function operate(operands, operators){
+
+    const final = operands.reduce((first, next, idx) => {
+
+        let operator = operators[idx-1]
+
+        switch (operator) {
+            case '+':
+                console.log(first, operator, next)
+                return add(first, next)
+            case '-':
+                console.log(first, operator, next)
+                return subtract(first, next)
+            case '*':
+                console.log(first, operator, next)
+                return multiply(first, next)
+            case '/':
+                console.log(first, operator, next)
+                return divide(first, next)
+        }       
+
+    })
+    console.log(final);
+    return final 
 
 }
 
 const display = document.querySelector('.calculator-display');
-
-const numbers = document.querySelectorAll('button.num');
-const operators = document.querySelectorAll('button.op');
+const buttons = document.querySelectorAll('button.op, button.num');
+ 
+let x = 0
+    ,y = 0
+    ,op;
 
 document.querySelector('.clear').addEventListener('click', 
-    () => display.textContent = '')
+    () => {
+        display.textContent = '';
+        x = 0;
+        y = 0;
+})
 
-numbers.forEach((button) => {
-    button.addEventListener('click', () => {
-        display.textContent += button.textContent;
-    });
-});
+let result = 0
+buttons.forEach(button => {
+    button.addEventListener('click', () => { 
+        if (button.textContent == '=') {
+            let eq =  document.querySelector('.calculator-display').textContent;
+            let operands = document.querySelector('.calculator-display').textContent.split(/\+|\-|\*|\//g);
+            let operators = eq.replace(/\d+/g,'').split('');
+            result = operate(operands, operators);
+            display.textContent = result;
+        } 
+        else {
+            display.textContent += button.textContent;
+        }
 
-operators.forEach(button => {
-    button.addEventListener('click', () => {
-        let x = +display.textContent;
-        let op = button.textContent;
+        
     });
 });
